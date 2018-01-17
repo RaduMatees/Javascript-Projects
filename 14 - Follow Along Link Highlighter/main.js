9c3a98278c312fd1282c9cf584f873927a8906d4
Radu Matees <radumatees@gmail.com>
2018-01-17 19:59:38 +0200

    14 - Follow along link highl

---
+++ 14 - Follow Along Link Highlighter/main.js
@@ -0,0 +1,14 @@
+const triggers = document.querySelectorAll('a')
+const highlight = document.createElement('span')
+highlight.classList.add('highlight')
+document.body.append(highlight)
+
+function highlightLink(){
+  const linkCoords = this.getBoundingClientRect()
+  console.log(linkCoords)
+  highlight.style.width = `${linkCoords.width}px`
+  highlight.style.height = `${linkCoords.height}px`
+  highlight.style.transform = `translate(${linkCoords.left + window.scrollX}px, ${linkCoords.top + window.scrollY}px)`
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', highlightLink))
