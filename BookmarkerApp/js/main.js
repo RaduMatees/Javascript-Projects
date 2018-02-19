7f22d18f089147839b50130bbebfb5bcc055a142
Radu Matees <radumatees@gmail.com>
2018-02-19 21:14:07 +0200

    add bookmark project and change readme file

---
+++ BookmarkerApp/js/main.js
@@ -0,0 +1,63 @@
+const form = document.querySelector('#myForm')
+const results = document.querySelector('#bookmarksResults')
+let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || []
+
+function saveBookmark(e){
+  e.preventDefault()
+  const siteName = form.querySelector('#siteName').value
+  const siteUrl = form.querySelector('#siteUrl').value
+
+  if (!validateForm(siteUrl)) return false
+
+  let bookmark = {
+    name: siteName,
+    url: siteUrl
+  }
+  bookmarks.push(bookmark)
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+
+  populatePage(results, bookmarks)
+  this.reset()
+}
+
+function populatePage(container, list){
+  results.innerHTML = list.map((bookmark, index) => {
+    return `
+      <div class='well' data-index='${index}'>
+        <h3>${bookmark.name}
+          <a class='btn btn-default' target='_blank' href='${bookmark.url}'>Visit</a>
+          <a class='btn btn-danger' href='#' data-index='${index}'>Delete</a>
+        </h3>
+      </div>
+    `
+  }).join('')
+}
+
+function deleteBookmark(e){
+  e.preventDefault()
+
+  // Remove the delete button parent DIV
+  if (!e.target.matches('a[class="btn btn-danger"]')) return
+  const btnIndex = e.target.dataset.index
+  const targetedWell = document.querySelector(`div[data-index='${btnIndex}']`)
+  targetedWell.remove()
+
+  // Update bookmarks list and local storage
+  bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+  bookmarks.splice(btnIndex, 1)
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+}
+
+function validateForm(url){
+  // validate for correct URL
+  let regex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi)
+  if (!url.match(regex)){
+    alert('Please fill in a valid URL')
+    return false
+  }
+  return true
+}
+
+populatePage(results, bookmarks)
+form.addEventListener('submit', saveBookmark)
+results.addEventListener('click', deleteBookmark)
--- README.md
+++ README.md
@@ -1,16 +1,8 @@
 
-JavaScript30
+JavaScript Projects
 ===
 
-Grab the course at https://JavaScript30.com
-
-All projects are created by **wesbos**
-
 ---
-
-  This repository contains projects from wesbos's Javascript30 course. After I
-completed the course, I decided recreating each project myself with the things
-I have learned.
 
   The main idea was becoming comfortable with Vanilla JavaScript and working
 with the DOM without a library. The project covers a large area of
