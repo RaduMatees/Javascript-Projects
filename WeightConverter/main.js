4885c6c22f2ca90dc3d295136194f2a81f6d44dc
Radu Matees <radumatees@gmail.com>
2018-02-24 21:04:59 +0200

    Improve the Weight Converter

--- WeightConverter/main.js
+++ WeightConverter/main.js
@@ -1,11 +1,52 @@
-const input = document.querySelector('#lbsInput');
-const output = document.querySelector('#output');
-output.style.visibility = 'hidden';
+const input = document.querySelector('#input');
+const outputs = [document.querySelector('#gramsOutput'), document.querySelector('#kgOutput'),
+document.querySelector('#ozOutput'), document.querySelector('#lbsOutput')];
+const cards = document.querySelectorAll('.card');
+input.disabled = true;
+let measUnit;
+
+cards.forEach(card => card.addEventListener('click', function(){
+  input.disabled = false;
+  measUnit = this.dataset.mu;
+  input.placeholder = `Enter ${measUnit}:`;
+  // if it's not the first click on a card
+  if (input.value) {
+    outputs.map((output, i) => output.innerHTML = resolve(input.value, measUnit)[i]);
+  }
+}))
 
 input.addEventListener('input', function() {
-  output.style.visibility = 'visible';
-  let lbs = this.value;
-  document.querySelector('#gramsOutput').innerHTML = (lbs/0.0022046).toFixed(2);
-  document.querySelector('#kgOutput').innerHTML = (lbs/2.2046).toFixed(2);
-  document.querySelector('#ozOutput').innerHTML = lbs*16;
+  let value = this.value;
+  outputs.map((output, i) => output.innerHTML = resolve(value, measUnit)[i]);
 })
+
+function resolve(userInput, userUnit){
+  let results = [];
+  let grams, kilograms, ounces, pounds;
+  if (userUnit === 'pounds'){
+    grams = userInput/0.0022046;
+    kilograms = userInput/2.2046;
+    ounces = userInput*16;
+    pounds = userInput;
+  }
+  if (userUnit === 'grams'){
+    grams = userInput;
+    kilograms = userInput/1000;
+    ounces = userInput*0.035274;
+    pounds = userInput*0.00220462262185;
+  }
+  if (userUnit === 'kilograms'){
+    grams = userInput*1000;
+    kilograms = userInput;
+    ounces = userInput*35.27396195;
+    pounds = userInput*2.20462262185;
+  }
+  if (userUnit === 'ounces'){
+    grams = userInput/0.035274;
+    kilograms = userInput/35.27396195;
+    ounces = userInput;
+    pounds = userInput*0.0625;
+  }
+  results.push(grams, kilograms, ounces, pounds);
+  return results;
+}
